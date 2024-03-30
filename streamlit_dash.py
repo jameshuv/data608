bc9c6721b08994d50b0920125e7e358f2667800c
hurdg <gavinhurd11@gmail.com>
2024-03-30 17:37:26 -0600

    Incorporated new traffic dtection csv

--- streamlit_dash.py
+++ streamlit_dash.py
@@ -25,11 +25,11 @@ col1, col2 = st.columns([0.2, 0.8])
 ########################################################################################
 ##################### Example Data Viz #################################################
 ########################################################################################
-chart_data = pd.read_csv('traffic_detection.csv')
+chart_data = pd.read_csv('traffic_detection-2.csv')
 chart_data.rename(columns = {'camera_location': 'Intersection'}, inplace = True)
 chart_data['traffic_sum'] = chart_data[['car','truck','bus']].sum(axis = 1)
 chart_data['Traffic'] = pd.cut(chart_data['traffic_sum'], 
-                            bins = chart_data['traffic_sum'].quantile([0,.33, .67,1]),
+                            bins = chart_data['traffic_sum'].quantile([0,.3, .7,1]),
                             labels = ['Low', 'Moderate', 'High'])
 
 #Customize sidebar
@@ -38,7 +38,7 @@ st.sidebar.write(chart_data[['Intersection', 'Traffic']])
 
 #Populate col 1
 with col1:
-    labels = list(chart_data['Traffic'].unique())
+    labels = list(chart_data['Traffic'].dropna().unique())
     values = list(chart_data['Traffic'].value_counts())
 
         
@@ -48,11 +48,11 @@ with col1:
                         hole=.4, hoverinfo="label+value+name", textinfo='percent+label',
                         marker=dict(colors=['darkorange', 'lightyellow', 'darkred'], line=dict(color='#000000', width=2))), 
                     row=1, col=1) 
-    n=16
-    x_example = np.arange(n)
+    n=16 #example hours
+    x_example = np.arange(n*12)
     fig.add_trace(go.Scatter(
-        x = pd.date_range('2024-03-26', periods=24, freq='1h'),
-        y = np.sin(4*np.pi*x_example/n)+np.cos(8*np.pi*x_example/n) + 100,
+        x = pd.date_range('2024-04-01', periods=24*12, freq='5min'),
+        y = np.sin(1/2*np.pi*x_example/n)+np.cos(1/4*np.pi*x_example/n) + 100,
         marker=dict(color='grey'),
         name="Historical Traffic",
         mode = 'lines'
